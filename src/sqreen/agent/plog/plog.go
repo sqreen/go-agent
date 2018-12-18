dc7b00076270b3a6c92bcec3a478e76e4430324b
Julio Guerra <julio@sqreen.io>
2018-12-18 11:34:04 +0100

    agent/plog: fast agent logger
    Implement a logger targeting agent needs:
    
    - fast when disabled.
    
    - provide several logger domains to individually enable/disable them (eg. hookpoint
      logger, rules logger, etc.).
    
    - provide several log levels, from Info to Fatal, to be able to scale the
      verbosity accordingly.
    
    - programmable logger outputs: use the `io.Writer` interface to be able to log
      to such objects.
    
    The approach for now only uses Go pointers to interfaces to replace them by
    either a disabled or enabled logger. The disabled one is made of empty methods,
    while the enable one really performs logs to the provided output.
    
    Test the logger package when enabled and disabled, along with relative
    performance comparison of enabled vs disabled to make sure it goes faster when
    disabled.

---
+++ src/sqreen/agent/plog/plog.go
@@ -0,0 +1,251 @@
+// Implementation of simple logging interfaces efficient in production
+// environments, aiming at being as fast as possible when disabled. The trick
+// consists in changing the underlying implementation pointer with a disabled
+// logger which does nothing when called. The call when disabled costs the
+// underlying interface call indirection, equivalent to 2 method calls.
+
+package plog
+
+import (
+	"fmt"
+	"io"
+	"log"
+)
+
+// LogLevel represents the log level. Higher levels include lowers.
+type LogLevel int
+
+const (
+	// Disabled value.
+	Disabled LogLevel = iota
+	// Fatal logs.
+	Fatal
+	// Error and Fatal logs.
+	Error
+	// Warn to Fatal logs.
+	Warn
+	// Info to Fatal logs.
+	Info
+)
+
+// LogLevel type stringer.
+func (l LogLevel) String() string {
+	switch l {
+	case Fatal:
+		return "fatal"
+	case Error:
+		return "error"
+	case Warn:
+		return "warn"
+	case Info:
+		return "info"
+	}
+	return ""
+}
+
+// Logger structure wrapping logger interfaces, one per level.
+type Logger struct {
+	FatalLogger
+	ErrorLogger
+	WarnLogger
+	InfoLogger
+
+	output    io.Writer
+	namespace string
+	cache     struct {
+		fatal, error, warn, info logger
+	}
+}
+
+type FatalLogger interface {
+	Fatal(v ...interface{})
+	Fatalf(format string, v ...interface{})
+	OutputSetter
+}
+
+type ErrorLogger interface {
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+	OutputSetter
+}
+
+type WarnLogger interface {
+	Warn(v ...interface{})
+	Warnf(format string, v ...interface{})
+	OutputSetter
+}
+
+type InfoLogger interface {
+	Info(v ...interface{})
+	Infof(format string, v ...interface{})
+	OutputSetter
+}
+
+type OutputSetter interface {
+	SetOutput(output io.Writer)
+}
+
+// NewLogger returns a Logger instance wrapping one logger instance per level.
+// They can thus be individually enabled or disabled.
+func NewLogger(namespace string) *Logger {
+	logger := &Logger{
+		FatalLogger: disabledLogger{},
+		ErrorLogger: disabledLogger{},
+		WarnLogger:  disabledLogger{},
+		InfoLogger:  disabledLogger{},
+		namespace:   namespace,
+	}
+	loggers[namespace] = logger
+	return logger
+}
+
+// SetOutput sets the output of the logger. When `nil`, the logger is disabled
+// and equivalent to `SetLevel(Disabled)`.
+func (l *Logger) SetOutput(output io.Writer) {
+	l.output = output
+	if output == nil {
+		l.SetLevel(Disabled)
+		return
+	}
+	l.FatalLogger.SetOutput(output)
+	l.ErrorLogger.SetOutput(output)
+	l.WarnLogger.SetOutput(output)
+	l.InfoLogger.SetOutput(output)
+}
+
+// SetLevel changes the level of the logger to `level`, possibly disabling it
+// when `Disabled` is passed.
+func (l *Logger) SetLevel(level LogLevel) {
+	switch level {
+	case Disabled:
+		l.InfoLogger = disabledLogger{}
+		l.WarnLogger = disabledLogger{}
+		l.ErrorLogger = disabledLogger{}
+		l.FatalLogger = disabledLogger{}
+		break
+	case Info:
+		l.InfoLogger = l.getLogger(Info)
+		l.WarnLogger = l.getLogger(Warn)
+		l.ErrorLogger = l.getLogger(Error)
+		l.FatalLogger = l.getLogger(Fatal)
+		break
+	case Warn:
+		l.InfoLogger = disabledLogger{}
+		l.WarnLogger = l.getLogger(Warn)
+		l.ErrorLogger = l.getLogger(Error)
+		l.FatalLogger = l.getLogger(Fatal)
+		break
+	case Error:
+		l.InfoLogger = disabledLogger{}
+		l.WarnLogger = disabledLogger{}
+		l.ErrorLogger = l.getLogger(Error)
+		l.FatalLogger = l.getLogger(Fatal)
+		break
+	case Fatal:
+		l.InfoLogger = disabledLogger{}
+		l.WarnLogger = disabledLogger{}
+		l.ErrorLogger = disabledLogger{}
+		l.FatalLogger = l.getLogger(Fatal)
+		break
+	}
+}
+
+// *log.Logger format prefix: UTC time, date of the day and time with microseconds.
+const flags = log.LUTC | log.Ldate | log.Lmicroseconds
+
+// Map of loggers.
+var loggers = make(map[string]*Logger)
+
+// Enabled logger instance.
+type logger struct {
+	logger *log.Logger
+}
+
+// Return the logger instance. Create a new one if first time.
+func (l *Logger) getLogger(level LogLevel) logger {
+	cache := l.getCachedLogger(level)
+	if cache.logger == nil {
+		*cache = newLogger(l.namespace, level, l.output)
+	} else {
+		cache.SetOutput(l.output)
+	}
+	return *cache
+}
+
+// Return a new logger.
+func newLogger(namespace string, level LogLevel, output io.Writer) logger {
+	return logger{log.New(output, namespace+":"+level.String()+": ", flags)}
+}
+
+// Return the pointer to the cache entry.
+func (l *Logger) getCachedLogger(level LogLevel) *logger {
+	switch level {
+	case Info:
+		return &l.cache.info
+	case Warn:
+		return &l.cache.warn
+	case Error:
+		return &l.cache.error
+	case Fatal:
+		return &l.cache.fatal
+	}
+	return nil
+}
+
+func (l logger) Fatal(v ...interface{}) {
+	l.logger.Output(3, fmt.Sprint(v...))
+}
+
+func (l logger) Fatalf(format string, v ...interface{}) {
+	l.logger.Output(3, fmt.Sprintf(format, v...))
+}
+
+func (l logger) Info(v ...interface{}) {
+	l.logger.Output(3, fmt.Sprint(v...))
+}
+
+func (l logger) Infof(format string, v ...interface{}) {
+	l.logger.Output(3, fmt.Sprintf(format, v...))
+}
+
+func (l logger) Warn(v ...interface{}) {
+	l.logger.Output(3, fmt.Sprint(v...))
+}
+
+func (l logger) Warnf(format string, v ...interface{}) {
+	l.logger.Output(3, fmt.Sprintf(format, v...))
+}
+
+func (l logger) Error(v ...interface{}) {
+	l.logger.Output(3, fmt.Sprint(v...))
+}
+
+func (l logger) Errorf(format string, v ...interface{}) {
+	l.logger.Output(3, fmt.Sprintf(format, v...))
+}
+
+func (l logger) SetOutput(output io.Writer) {
+	l.logger.SetOutput(output)
+}
+
+type disabledLogger struct {
+}
+
+func (_ disabledLogger) Fatal(_ ...interface{}) {
+}
+func (_ disabledLogger) Fatalf(_ string, _ ...interface{}) {
+}
+func (_ disabledLogger) Error(_ ...interface{}) {
+}
+func (_ disabledLogger) Errorf(_ string, _ ...interface{}) {
+}
+func (_ disabledLogger) Warn(_ ...interface{}) {
+}
+func (_ disabledLogger) Warnf(_ string, _ ...interface{}) {
+}
+func (_ disabledLogger) Info(_ ...interface{}) {
+}
+func (_ disabledLogger) Infof(_ string, _ ...interface{}) {
+}
+func (_ disabledLogger) SetOutput(_ io.Writer) {
+}
