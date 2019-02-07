4786737a7c5385f670e7fd0d16076d159e65cf3e
Julio Guerra <guerra@julio.in>
2019-02-07 12:00:20 +0100

    agent/config: configuration file location test

--- tools/testlib/rand.go
+++ tools/testlib/rand.go
@@ -2,10 +2,17 @@ package testlib
 
 import "math/rand"
 
-func RandString(from, to int) string {
+func RandString(size ...int) string {
 	letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-	n := from + rand.Intn(to-from)
+	var n int
+	if len(size) == 1 {
+		n = size[0]
+	} else {
+		from := size[0]
+		to := size[1]
+		n = from + rand.Intn(to-from)
+	}
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
