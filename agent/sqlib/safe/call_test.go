0d214ad560c86537dc5d20244548d389a2b812bd
Julio Guerra <julio@sqreen.com>
2019-05-07 21:44:04 +0200

    agent/sqlib/safe: add panic-safe function and goroutine wrappers
    This package provides functions making sure panics are safely caught and do not
    break the running program. Since panics can stop the program execution if they
    are not recovered, we need ways to safely recover and handle them.
    
    Therefore, this package provides simple function call wrappers to call a
    function that may panic and its goroutine equivalent. That way, any function can
    be safely called and any panic will be returned as a regular function error.

---
+++ agent/sqlib/safe/call_test.go
@@ -0,0 +1,75 @@
+// Copyright (c) 2016 - 2019 Sqreen. All Rights Reserved.
+// Please refer to our terms for more information:
+// https://www.sqreen.io/terms.html
+
+package safe_test
+
+import (
+	"testing"
+
+	"github.com/sqreen/go-agent/agent/sqlib"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/xerrors"
+)
+
+func TestCall(t *testing.T) {
+	t.Run("without error", func(t *testing.T) {
+		err := sqlib.Call(func() error {
+			return nil
+		})
+		require.NoError(t, err)
+	})
+
+	t.Run("with a regular error", func(t *testing.T) {
+		err := sqlib.Call(func() error {
+			return xerrors.New("oops")
+		})
+		require.Error(t, err)
+		require.Equal(t, "oops", err.Error())
+	})
+
+	t.Run("with a panic string error", func(t *testing.T) {
+		err := sqlib.Call(func() error {
+			panic("oops")
+			return nil
+		})
+		require.Error(t, err)
+		var panicErr *sqlib.PanicError
+		require.Error(t, err)
+		require.True(t, xerrors.As(err, &panicErr))
+		require.Equal(t, "oops", panicErr.Err.Error())
+	})
+
+	t.Run("with a panic error", func(t *testing.T) {
+		err := sqlib.Call(func() error {
+			panic(xerrors.New("oops"))
+			return nil
+		})
+		require.Error(t, err)
+		var panicErr *sqlib.PanicError
+		require.Error(t, err)
+		require.True(t, xerrors.As(err, &panicErr))
+		require.Equal(t, "oops", panicErr.Err.Error())
+	})
+
+	t.Run("with another panic argument type", func(t *testing.T) {
+		err := sqlib.Call(func() error {
+			panic(33.7)
+			return nil
+		})
+		require.Error(t, err)
+		var panicErr *sqlib.PanicError
+		require.Error(t, err)
+		require.True(t, xerrors.As(err, &panicErr))
+		require.Equal(t, "33.7", panicErr.Err.Error())
+	})
+
+	t.Run("with a nil panic argument value", func(t *testing.T) {
+		err := sqlib.Call(func() error {
+			// This case cannot be differentiated yet.
+			panic(nil)
+			return xerrors.New("oops")
+		})
+		require.NoError(t, err)
+	})
+}
---
+++ agent/sqlib/safe/go.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2016 - 2019 Sqreen. All Rights Reserved.
+// Please refer to our terms for more information:
+// https://www.sqreen.io/terms.html
+
+package safe
+
+// Go mimics the `go` goroutine built-in to execute function `f` in a goroutine
+// but with the ability to safely recover from any panic occurring while it
+// executes. To do so, it uses `Call()` and returns an error channel in order
+// to retrieve any panic occurring during the execution of `f()` or the error
+// it returns otherwise. An error is sent into the channel only in case of
+// error or panic, and is closed in any case before returning from the
+// goroutine.
+//
+// Usage example:
+//
+//		errChan := safe.Go(f)
+//    // ...
+//		select {
+//			case err := <-errChan:
+//				var panicErr *sqlib.PanicError
+//				if xerrors.As(err, &panicErr) {
+//					// A panic occurred while executing f()
+//				} else {
+//					// A regular error was returned by f()
+//				}
+//			// ...
+//		}
+//
+func Go(f func() error) <-chan error {
+	c := make(chan error, 1)
+	go func() {
+		if err := Call(f); err != nil {
+			c <- err
+		}
+		close(c)
+	}()
+	return c
+}
