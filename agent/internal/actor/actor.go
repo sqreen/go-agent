de053add2b1dbb4792e4858fa206914184313ec5
Julio Guerra <julio@sqreen.com>
2019-05-07 11:37:51 +0200

    doc: add copyrights everywhere

--- agent/internal/actor/actor.go
+++ agent/internal/actor/actor.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Sqreen. All Rights Reserved.
+// Copyright (c) 2016 - 2019 Sqreen. All Rights Reserved.
 // Please refer to our terms for more information:
 // https://www.sqreen.io/terms.html
 
--- agent/internal/backend/api/jsonpb.go
+++ agent/internal/backend/api/jsonpb.go
@@ -1,8 +1,12 @@
+// Copyright (c) 2016 - 2019 Sqreen. All Rights Reserved.
+// Please refer to our terms for more information:
+// https://www.sqreen.io/terms.html
+
 package api
 
 import (
 	"encoding/json"
-	fmt "fmt"
+	"fmt"
 )
 
 var RequestRecordVersion = "20171208"
