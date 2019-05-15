ddf93e6ef25602f9f7b8b75e4dacdc114812648d
Julio Guerra <julio@sqreen.com>
2019-05-15 16:55:24 +0200

    Error management: add panic-safe function and goroutine wrappers
    This package provides functions making sure panics are safely caught and do not
    break the running program. Since panics can stop the program execution if they
    are not recovered, we need ways to safely recover and handle them.
    
    Therefore, this package provides simple function call wrappers to call a
    function that may panic and its goroutine equivalent. That way, any function can
    be safely called and any panic will be returned as a regular function error.

---
+++ agent/sqlib/safe/call.go
@@ -0,0 +1,65 @@
+// Copyright (c) 2016 - 2019 Sqreen. All Rights Reserved.
+// Please refer to our terms for more information:
+// https://www.sqreen.io/terms.html
+
+package safe
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+
+	"golang.org/x/xerrors"
+)
+
+// PanicError is an error type wrapping a recovered panic value that happened
+// during a function call.
+type PanicError struct {
+	// The function that was given to `Call()`.
+	In func() error
+	// The recovered panic value while executing `In()`.
+	Err error
+}
+
+func NewPanicError(in func() error, err error) *PanicError {
+	return &PanicError{
+		In:  in,
+		Err: err,
+	}
+}
+
+func (e PanicError) Unwrap() error {
+	return e.Err
+}
+
+func (e PanicError) inName() string {
+	return runtime.FuncForPC(reflect.ValueOf(e.In).Pointer()).Name()
+}
+
+func (e PanicError) Error() string {
+	return fmt.Sprintf("panic while executing %s: %s", e.inName(), e.Err)
+}
+
+// Call calls function `f` and recovers from any panic occurring while it
+// executes, returning it in a `PanicError` object type.
+func Call(f func() error) (err error) {
+	defer func() {
+		r := recover()
+		if r == nil {
+			// Note that panic(nil) matches this case and cannot be really tested for.
+			return
+		}
+
+		switch actual := r.(type) {
+		case error:
+			err = actual
+		case string:
+			err = xerrors.New(actual)
+		default:
+			err = xerrors.Errorf("%v", r)
+		}
+
+		err = NewPanicError(f, err)
+	}()
+	return f()
+}
