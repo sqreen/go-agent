07f5df05a4ea34a502a89358ea5418fdf803ae90
Julio Guerra <julio@sqreen.com>
2019-09-09 09:45:51 +0200

    agent: add access to the request record from hookpoint callbacks
    Dummy refactoring of request records into their own package with the ability to
    retrieve them from callbacks. To do so, the internal request record pointer is
    added to the request's context. A callback can access the request record if it
    has access to the request. Accessing the request record from requests allows to
    add events such as attack event reports.

--- tools/testlib/agent.go
+++ tools/testlib/agent.go
@@ -6,9 +6,9 @@ package testlib
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/sqreen/go-agent/agent/types"
+	"github.com/sqreen/go-agent/tools/testlib/testmock"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -25,9 +25,9 @@ func (a *AgentMockup) ResetExpectations() {
 	}
 }
 
-func (a *AgentMockup) NewRequestRecord(r *http.Request) types.RequestRecord {
+func (a *AgentMockup) NewRequestRecord(r *http.Request) (types.RequestRecord, *http.Request) {
 	ret := a.Called(r)
-	return ret.Get(0).(types.RequestRecord)
+	return ret.Get(0).(types.RequestRecord), r
 }
 
 func (a *AgentMockup) ExpectNewRequestRecord(r interface{}) *mock.Call {
@@ -42,9 +42,9 @@ func (a *AgentMockup) ExpectGracefulStop() *mock.Call {
 	return a.On("GracefulStop")
 }
 
-func NewAgentForMiddlewareTestsWithoutSecurityResponse() (*AgentMockup, *HTTPRequestRecordMockup) {
+func NewAgentForMiddlewareTestsWithoutSecurityResponse() (*AgentMockup, *testmock.RequestRecordMockup) {
 	agent := &AgentMockup{}
-	record := &HTTPRequestRecordMockup{}
+	record := &testmock.RequestRecordMockup{}
 	agent.ExpectNewRequestRecord(mock.Anything).Return(record).Once()
 	record.ExpectWhitelisted().Return(false).Once()
 	record.ExpectSecurityResponse().Return(nil).Once()
@@ -53,9 +53,9 @@ func NewAgentForMiddlewareTestsWithoutSecurityResponse() (*AgentMockup, *HTTPReq
 	return agent, record
 }
 
-func NewAgentForMiddlewareTestsWithSecurityResponse(actionHandler http.Handler) (*AgentMockup, *HTTPRequestRecordMockup) {
+func NewAgentForMiddlewareTestsWithSecurityResponse(actionHandler http.Handler) (*AgentMockup, *testmock.RequestRecordMockup) {
 	agent := &AgentMockup{}
-	record := &HTTPRequestRecordMockup{}
+	record := &testmock.RequestRecordMockup{}
 	agent.ExpectNewRequestRecord(mock.Anything).Return(record).Once()
 	record.ExpectWhitelisted().Return(false).Once()
 	record.ExpectSecurityResponse().Return(actionHandler).Once()
@@ -63,117 +63,13 @@ func NewAgentForMiddlewareTestsWithSecurityResponse(actionHandler http.Handler) 
 	return agent, record
 }
 
-func NewAgentForMiddlewareTestsWithUserSecurityResponse(actionHandler http.Handler) (*AgentMockup, *HTTPRequestRecordMockup) {
+func NewAgentForMiddlewareTestsWithUserSecurityResponse(actionHandler http.Handler) (*AgentMockup, *testmock.RequestRecordMockup) {
 	agent := &AgentMockup{}
-	record := &HTTPRequestRecordMockup{}
+	record := &testmock.RequestRecordMockup{}
 	agent.ExpectNewRequestRecord(mock.Anything).Return(record).Once()
 	record.ExpectWhitelisted().Return(false).Once()
 	record.ExpectSecurityResponse().Return(nil).Once()
 	record.ExpectUserSecurityResponse().Return(actionHandler)
 	record.ExpectClose().Once()
 	return agent, record
-}
-
-type HTTPRequestRecordMockup struct {
-	mock.Mock
-}
-
-// Static assertion of correct interface implementation.
-var _ types.RequestRecord = &HTTPRequestRecordMockup{}
-
-func (r *HTTPRequestRecordMockup) NewCustomEvent(event string) types.CustomEvent {
-	r.Called(event)
-	return r
-}
-
-func (r *HTTPRequestRecordMockup) ExpectTrackEvent(event string) *mock.Call {
-	return r.On("NewCustomEvent", event)
-}
-
-func (r *HTTPRequestRecordMockup) Whitelisted() bool {
-	return r.Called().Bool(0)
-}
-
-func (r *HTTPRequestRecordMockup) ExpectWhitelisted() *mock.Call {
-	return r.On("Whitelisted")
-}
-
-func (r *HTTPRequestRecordMockup) Close() {
-	r.Called()
-}
-
-func (r *HTTPRequestRecordMockup) ExpectClose() *mock.Call {
-	return r.On("Close")
-}
-
-func (r *HTTPRequestRecordMockup) NewUserAuth(id map[string]string, success bool) {
-	r.Called(id, success)
-}
-
-func (r *HTTPRequestRecordMockup) ExpectTrackAuth(id map[string]string, success bool) *mock.Call {
-	return r.On("NewUserAuth", id, success)
-}
-
-func (r *HTTPRequestRecordMockup) NewUserSignup(id map[string]string) {
-	r.Called(id)
-}
-
-func (r *HTTPRequestRecordMockup) ExpectTrackSignup(id map[string]string) *mock.Call {
-	return r.On("NewUserSignup", id)
-}
-
-func (r *HTTPRequestRecordMockup) Identify(id map[string]string) {
-	r.Called(id)
-}
-
-func (r *HTTPRequestRecordMockup) SecurityResponse() http.Handler {
-	ret := r.Called().Get(0)
-	if ret == nil {
-		return nil
-	}
-	return ret.(http.Handler)
-}
-
-func (r *HTTPRequestRecordMockup) ExpectSecurityResponse() *mock.Call {
-	return r.On("SecurityResponse")
-}
-
-func (r *HTTPRequestRecordMockup) UserSecurityResponse() http.Handler {
-	ret := r.Called().Get(0)
-	if ret == nil {
-		return nil
-	}
-	return ret.(http.Handler)
-}
-
-func (r *HTTPRequestRecordMockup) ExpectUserSecurityResponse() *mock.Call {
-	return r.On("UserSecurityResponse")
-}
-
-func (r *HTTPRequestRecordMockup) ExpectIdentify(id map[string]string) *mock.Call {
-	return r.On("Identify", id)
-}
-
-func (r *HTTPRequestRecordMockup) WithTimestamp(t time.Time) {
-	r.Called(t)
-}
-
-func (r *HTTPRequestRecordMockup) ExpectWithTimestamp(t time.Time) *mock.Call {
-	return r.On("WithTimestamp", t)
-}
-
-func (r *HTTPRequestRecordMockup) WithProperties(props types.EventProperties) {
-	r.Called(props)
-}
-
-func (r *HTTPRequestRecordMockup) ExpectWithProperties(props types.EventProperties) *mock.Call {
-	return r.On("WithProperties", props)
-}
-
-func (r *HTTPRequestRecordMockup) WithUserIdentifiers(id map[string]string) {
-	r.Called(id)
-}
-
-func (r *HTTPRequestRecordMockup) ExpectWithUserIdentifiers(id map[string]string) *mock.Call {
-	return r.On("WithUserIdentifiers", id)
 }
