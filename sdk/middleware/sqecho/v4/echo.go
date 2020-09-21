47c8c7c54504a55f0390b834a3ef5edb6d0b213e
Julio Guerra <julio@sqreen.com>
2020-09-21 22:31:15 +0200

    sdk/middleware/sqecho/v4: add v4 support

---
+++ sdk/middleware/sqecho/v4/echo.go
@@ -0,0 +1,295 @@
+// Copyright (c) 2016 - 2019 Sqreen. All Rights Reserved.
+// Please refer to our terms for more information:
+// https://www.sqreen.io/terms.html
+
+package sqecho
+
+import (
+	"bytes"
+	"io"
+	"net"
+	"net/http"
+	"net/textproto"
+	"net/url"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sqreen/go-agent/internal"
+	protectioncontext "github.com/sqreen/go-agent/internal/protection/context"
+	http_protection "github.com/sqreen/go-agent/internal/protection/http"
+	"github.com/sqreen/go-agent/internal/protection/http/types"
+	"github.com/sqreen/go-agent/sdk"
+)
+
+// FromContext allows to access the HTTPRequestRecord from Echo request handlers
+// if present, and nil otherwise. The value is stored in handler contexts by the
+// middleware function, and is of type *HTTPRequestRecord.
+//
+// Note that Echo's context does not implement the `context.Context` interface,
+// so `sdk.FromContext()` cannot be used with it, hence another `FromContext()`
+// in this package to access the SDK context value from Echo's context.
+// `sdk.FromContext()` can still be used on the request context.
+func FromContext(c echo.Context) *sdk.Context {
+	return sdk.FromContext(c.Request().Context())
+}
+
+// Middleware is Sqreen's middleware function for echo to monitor and protect the
+// requests echo receives.
+//
+// SDK methods can be called from request handlers by using the request context.
+// It can be retrieved from the request context using `sdk.FromContext()` or
+// on a echo's context.
+//
+// Usage example:
+//
+//	e := echo.New()
+//	e.Use(sqecho.Middleware())
+//
+//	e.GET("/", func(c echo.Context) error {
+//		// Accessing the SDK through Echo's context
+//		sqecho.FromContext(c).TrackEvent("my.event.one")
+//		foo(c.Request())
+//		return nil
+//	}
+//
+//	func foo(req *http.Request) {
+//		// Accessing the SDK through the request context
+//		sdk.FromContext(req.Context()).TrackEvent("my.event.two")
+//	}
+//
+//	e.GET("/", func(c echo.Context) {
+//		// Globally identifying a user and checking if the request should be
+//		// aborted.
+//		uid := sdk.EventUserIdentifiersMap{"uid": "my-uid"}
+//		sqUser := sqecho.FromContext(c).ForUser(uid)
+//		// Globally associate this user to the current request and check if it got
+//		// blocked.
+//		if err := sqUser.Identify(); err != nil {
+//			// Return to stop further handling the request
+//			return err
+//		}
+//		// ... not blocked ...
+//		return nil
+//	}
+//
+func Middleware() echo.MiddlewareFunc {
+	internal.Start()
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			return middlewareHandler(internal.Agent(), next, c)
+		}
+	}
+}
+
+func middlewareHandler(agent protectioncontext.AgentFace, next echo.HandlerFunc, c echo.Context) error {
+	if agent == nil {
+		return next(c)
+	}
+
+	requestReader := &requestReaderImpl{c: c}
+	responseWriter := &responseWriterImpl{c: c}
+
+	req := c.Request()
+
+	ctx, reqCtx, cancelHandlerContext := http_protection.NewRequestContext(req.Context(), agent, responseWriter, requestReader)
+	if ctx == nil {
+		return next(c)
+	}
+
+	defer func() {
+		cancelHandlerContext()
+		_ = ctx.Close(responseWriter.closeResponseWriter())
+	}()
+
+	req = ctx.WrapRequest(reqCtx, req)
+	c.SetRequest(req)
+	c.Set(protectioncontext.ContextKey.String, ctx)
+
+	if err := ctx.Before(); err != nil {
+		return err
+	}
+	if err := next(c); err != nil {
+		// Handler-based protection such as user security responses or RASP
+		// protection may lead to aborted requests bubbling up the error that
+		// was returned.
+		return err
+	}
+	if err := ctx.After(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type requestReaderImpl struct {
+	c              echo.Context
+	bodyReadBuffer bytes.Buffer
+	queryForm      url.Values
+}
+
+func (r *requestReaderImpl) Body() []byte {
+	return nil
+}
+
+func (r *requestReaderImpl) UserAgent() string {
+	return r.c.Request().UserAgent()
+}
+
+func (r *requestReaderImpl) Referer() string {
+	return r.c.Request().Referer()
+}
+
+func (r *requestReaderImpl) ClientIP() net.IP {
+	return nil // Delegated to the middleware according the agent configuration
+}
+
+func (r *requestReaderImpl) Method() string {
+	return r.c.Request().Method
+}
+
+func (r *requestReaderImpl) URL() *url.URL {
+	return r.c.Request().URL
+}
+
+func (r *requestReaderImpl) RequestURI() string {
+	return r.c.Request().RequestURI
+}
+
+func (r *requestReaderImpl) Host() string {
+	return r.c.Request().Host
+}
+
+func (r *requestReaderImpl) IsTLS() bool {
+	return r.c.IsTLS()
+}
+
+func (r *requestReaderImpl) Params() types.RequestParamMap {
+	params := r.c.ParamNames()
+	l := len(params)
+	if l == 0 {
+		return nil
+	}
+
+	m := make(types.RequestParamMap, l)
+	for _, key := range params {
+		m.Add(key, r.c.Param(key))
+	}
+	return m
+}
+
+func (r *requestReaderImpl) QueryForm() url.Values {
+	if r.queryForm == nil {
+		r.queryForm = r.c.Request().URL.Query()
+	}
+	return r.queryForm
+}
+
+func (r *requestReaderImpl) PostForm() url.Values {
+	return r.c.Request().PostForm
+}
+
+func (r *requestReaderImpl) Headers() http.Header {
+	return r.c.Request().Header
+}
+
+func (r *requestReaderImpl) Header(h string) *string {
+	headers := r.c.Request().Header
+	if headers == nil {
+		return nil
+	}
+	v := headers[textproto.CanonicalMIMEHeaderKey(h)]
+	if len(v) == 0 {
+		return nil
+	}
+	return &v[0]
+}
+
+func (r *requestReaderImpl) RemoteAddr() string {
+	return r.c.Request().RemoteAddr
+}
+
+type responseWriterImpl struct {
+	c      echo.Context
+	closed bool
+}
+
+func (w *responseWriterImpl) closeResponseWriter() types.ResponseFace {
+	if !w.closed {
+		w.closed = true
+	}
+	return newObservedResponse(w)
+}
+
+func (w *responseWriterImpl) Header() http.Header {
+	return w.c.Response().Header()
+}
+
+func (w *responseWriterImpl) Write(b []byte) (int, error) {
+	if w.closed {
+		return 0, types.WriteAfterCloseError{}
+	}
+	return w.c.Response().Write(b)
+}
+
+func (w *responseWriterImpl) WriteString(s string) (int, error) {
+	if w.closed {
+		return 0, types.WriteAfterCloseError{}
+	}
+	return io.WriteString(w.c.Response(), s)
+}
+
+// Static assert that the io.StringWriter is implemented
+var _ io.StringWriter = (*responseWriterImpl)(nil)
+
+func (w *responseWriterImpl) WriteHeader(statusCode int) {
+	if w.closed {
+		return
+	}
+	w.c.Response().WriteHeader(statusCode)
+}
+
+// response observed by the response writer
+type observedResponse struct {
+	contentType   string
+	contentLength int64
+	status        int
+}
+
+func newObservedResponse(r *responseWriterImpl) *observedResponse {
+	// Content-Type will be not empty only when explicitly set.
+	// It could be guessed as net/http does. Not implemented for now.
+	ct := r.Header().Get("Content-Type")
+
+	response := r.c.Response()
+
+	// Content-Length is either explicitly set or the amount of written data.
+	cl := response.Size
+	if contentLength := r.Header().Get("Content-Length"); contentLength != "" {
+		if l, err := strconv.ParseInt(contentLength, 10, 0); err == nil {
+			cl = l
+		}
+	}
+
+	status := response.Status
+
+	return &observedResponse{
+		contentType:   ct,
+		contentLength: cl,
+		status:        status,
+	}
+}
+
+func (r *observedResponse) Status() int {
+	if status := r.status; status != 0 {
+		return status
+	}
+	// Default net/http status is 200
+	return http.StatusOK
+}
+
+func (r *observedResponse) ContentType() string {
+	return r.contentType
+}
+
+func (r *observedResponse) ContentLength() int64 {
+	return r.contentLength
+}
---
+++ sdk/middleware/sqecho/v4/echo_test.go
@@ -0,0 +1,416 @@
+// Copyright (c) 2016 - 2019 Sqreen. All Rights Reserved.
+// Please refer to our terms for more information:
+// https://www.sqreen.io/terms.html
+
+package sqecho
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	protectioncontext "github.com/sqreen/go-agent/internal/protection/context"
+	"github.com/sqreen/go-agent/internal/protection/http/types"
+	"github.com/sqreen/go-agent/sdk"
+	"github.com/sqreen/go-agent/sdk/middleware/_testlib/mockups"
+	"github.com/sqreen/go-agent/tools/testlib"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMiddleware(t *testing.T) {
+	t.Run("without middleware", func(t *testing.T) {
+		body := testlib.RandUTF8String(4096)
+		req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(body))
+
+		// Create an Echo context
+		e := echo.New()
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		// Define a Echo handler
+		h := func(c echo.Context) error {
+			{
+				// using echo's context interface
+				sq := FromContext(c)
+				require.NotNil(t, sq)
+				sq.TrackEvent("my event").WithProperties(sdk.EventPropertyMap{"my": "prop"}).WithTimestamp(time.Now()).WithUserIdentifiers(sdk.EventUserIdentifiersMap{"my": "id"})
+				sq.ForUser(sdk.EventUserIdentifiersMap{"my": "id"}).TrackEvent("my event").WithProperties(sdk.EventPropertyMap{"my": "prop"}).WithTimestamp(time.Now())
+			}
+			{
+				// using echo's context interface
+				sq := sdk.FromContext(c.Request().Context())
+				require.NotNil(t, sq)
+				sq.TrackEvent("my event").WithProperties(sdk.EventPropertyMap{"my": "prop"}).WithTimestamp(time.Now()).WithUserIdentifiers(sdk.EventUserIdentifiersMap{"my": "id"})
+				sq.ForUser(sdk.EventUserIdentifiersMap{"my": "id"}).TrackEvent("my event").WithProperties(sdk.EventPropertyMap{"my": "prop"}).WithTimestamp(time.Now())
+			}
+			body, err := ioutil.ReadAll(c.Request().Body)
+			if err != nil {
+				return err
+			}
+			return c.String(http.StatusOK, string(body))
+		}
+		// Perform the request and record the output
+		err := h(c)
+		// Check the request was performed as expected
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rec.Code)
+		require.Equal(t, body, rec.Body.String())
+	})
+
+	t.Run("without middleware", func(t *testing.T) {
+		body := testlib.RandUTF8String(4096)
+		req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(body))
+
+		// Create an Echo context
+		e := echo.New()
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		// Define a Echo handler
+		h := func(c echo.Context) error {
+			{
+				// using echo's context interface
+				sq := FromContext(c)
+				require.NotNil(t, sq)
+				sq.TrackEvent("my event").WithProperties(sdk.EventPropertyMap{"my": "prop"}).WithTimestamp(time.Now()).WithUserIdentifiers(sdk.EventUserIdentifiersMap{"my": "id"})
+				sq.ForUser(sdk.EventUserIdentifiersMap{"my": "id"}).TrackEvent("my event").WithProperties(sdk.EventPropertyMap{"my": "prop"}).WithTimestamp(time.Now())
+			}
+			{
+				// using echo's context interface
+				sq := sdk.FromContext(c.Request().Context())
+				require.NotNil(t, sq)
+				sq.TrackEvent("my event").WithProperties(sdk.EventPropertyMap{"my": "prop"}).WithTimestamp(time.Now()).WithUserIdentifiers(sdk.EventUserIdentifiersMap{"my": "id"})
+				sq.ForUser(sdk.EventUserIdentifiersMap{"my": "id"}).TrackEvent("my event").WithProperties(sdk.EventPropertyMap{"my": "prop"}).WithTimestamp(time.Now())
+			}
+			body, err := ioutil.ReadAll(c.Request().Body)
+			if err != nil {
+				return err
+			}
+			return c.String(http.StatusOK, string(body))
+		}
+		// Perform the request and record the output
+		err := Middleware()(h)(c)
+		// Check the request was performed as expected
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rec.Code)
+		require.Equal(t, body, rec.Body.String())
+	})
+
+	t.Run("control flow", func(t *testing.T) {
+		middlewareResponseBody := testlib.RandUTF8String(4096)
+		middlewareResponseStatus := 433
+		handlerResponseBody := testlib.RandUTF8String(4096)
+		handlerResponseStatus := 533
+		agent := &mockups.AgentMockup{}
+		agent.ExpectConfig().Return(&mockups.AgentConfigMockup{})
+		agent.ExpectIsIPAllowed(mock.Anything).Return(false)
+		agent.ExpectIsPathAllowed(mock.Anything).Return(false)
+		agent.ExpectSendClosedRequestContext(mock.Anything).Return(nil)
+		defer agent.AssertExpectations(t) // inaccurate but worth it
+
+		for _, tc := range []struct {
+			name  string
+			agent protectioncontext.AgentFace
+		}{
+			{
+				name:  "agent disabled",
+				agent: nil,
+			},
+			{
+				name:  "agent enabled",
+				agent: agent,
+			},
+		} {
+			tc := tc
+			t.Run(tc.name, func(t *testing.T) {
+				for _, tc := range []struct {
+					name        string
+					middlewares []echo.MiddlewareFunc
+					handler     func(echo.Context) error
+					test        func(t *testing.T, rec *httptest.ResponseRecorder, err error)
+				}{
+					{
+						name: "sqreen first/the middleware aborts before the handler",
+						middlewares: []echo.MiddlewareFunc{
+							middleware(tc.agent),
+							func(next echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									c.String(middlewareResponseStatus, middlewareResponseBody)
+									return errors.New("middleware abort")
+								}
+							},
+						},
+						handler: func(echo.Context) error {
+							panic("unexpected control flow")
+							return nil
+						},
+						test: func(t *testing.T, rec *httptest.ResponseRecorder, err error) {
+							require.Error(t, err)
+							require.Equal(t, "middleware abort", err.Error())
+							require.Equal(t, middlewareResponseStatus, rec.Code)
+							require.Equal(t, middlewareResponseBody, rec.Body.String())
+						},
+					},
+
+					{
+						name: "sqreen first/the handler aborts",
+						middlewares: []echo.MiddlewareFunc{
+							middleware(tc.agent),
+							func(next echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									err := next(c)
+									require.Error(t, err)
+									return err
+								}
+							},
+						},
+						handler: func(c echo.Context) error {
+							c.String(handlerResponseStatus, handlerResponseBody)
+							return errors.New("handler abort")
+						},
+						test: func(t *testing.T, rec *httptest.ResponseRecorder, err error) {
+							require.Error(t, err)
+							require.Equal(t, "handler abort", err.Error())
+							require.Equal(t, handlerResponseStatus, rec.Code)
+							require.Equal(t, handlerResponseBody, rec.Body.String())
+						},
+					},
+
+					{
+						name: "sqreen first/no one aborts",
+						middlewares: []echo.MiddlewareFunc{
+							middleware(tc.agent),
+							func(next echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									err := c.String(middlewareResponseStatus, middlewareResponseBody)
+									require.NoError(t, err)
+									err = next(c)
+									require.NoError(t, err)
+									err = c.String(middlewareResponseStatus, middlewareResponseBody)
+									require.NoError(t, err)
+									return err
+								}
+							},
+						},
+						handler: func(c echo.Context) error {
+							return c.String(handlerResponseStatus, handlerResponseBody)
+						},
+						test: func(t *testing.T, rec *httptest.ResponseRecorder, err error) {
+							require.NoError(t, err)
+							require.Equal(t, middlewareResponseStatus, rec.Code)
+							require.Equal(t, middlewareResponseBody+handlerResponseBody+middlewareResponseBody, rec.Body.String())
+						},
+					},
+
+					{
+						name: "sqreen first/the middleware aborts after the handler",
+						middlewares: []echo.MiddlewareFunc{
+							middleware(tc.agent),
+							func(next echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									err := next(c)
+									require.NoError(t, err)
+									err = c.String(middlewareResponseStatus, middlewareResponseBody)
+									require.NoError(t, err)
+									return errors.New("middleware abort")
+								}
+							},
+						},
+						handler: func(c echo.Context) error {
+							return nil
+						},
+						test: func(t *testing.T, rec *httptest.ResponseRecorder, err error) {
+							require.Error(t, err)
+							require.Equal(t, "middleware abort", err.Error())
+							require.Equal(t, middlewareResponseStatus, rec.Code)
+							require.Equal(t, middlewareResponseBody, rec.Body.String())
+						},
+					},
+
+					{
+						name: "sqreen after another middleware/issue AGO-29: the middleware aborts after the next handler",
+						middlewares: []echo.MiddlewareFunc{
+							func(next echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									err := next(c)
+									require.NoError(t, err)
+									err = c.String(middlewareResponseStatus, middlewareResponseBody)
+									require.NoError(t, err)
+									return errors.New("middleware abort")
+								}
+							},
+							middleware(tc.agent),
+						},
+						handler: func(c echo.Context) error {
+							// Do nothing so that the calling middleware can handle the response.
+							return nil
+						},
+						test: func(t *testing.T, rec *httptest.ResponseRecorder, err error) {
+							require.Error(t, err)
+							require.Equal(t, "middleware abort", err.Error())
+							require.Equal(t, middlewareResponseStatus, rec.Code)
+							require.Equal(t, middlewareResponseBody, rec.Body.String())
+						},
+					},
+
+					{
+						name: "sqreen after another middleware/the middleware aborts before the next handler",
+						middlewares: []echo.MiddlewareFunc{
+							func(echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									err := c.String(middlewareResponseStatus, middlewareResponseBody)
+									require.NoError(t, err)
+									return errors.New("middleware abort")
+								}
+							},
+							func(echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									// Make sure echo doesn't call the next middleware when the
+									// previous one returns an error.
+									panic("unexpected control flow")
+								}
+							},
+							middleware(tc.agent),
+						},
+						handler: func(echo.Context) error {
+							// Make sure echo doesn't call the handler when one of the
+							// previous middlewares return an error.
+							panic("unexpected control flow")
+						},
+						test: func(t *testing.T, rec *httptest.ResponseRecorder, err error) {
+							require.Error(t, err)
+							require.Equal(t, "middleware abort", err.Error())
+							require.Equal(t, middlewareResponseStatus, rec.Code)
+							require.Equal(t, middlewareResponseBody, rec.Body.String())
+						},
+					},
+
+					{
+						name: "sqreen after another middleware/the handler aborts",
+						middlewares: []echo.MiddlewareFunc{
+							func(next echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									err := next(c)
+									require.Error(t, err)
+									return err
+								}
+							},
+							middleware(tc.agent),
+						},
+						handler: func(c echo.Context) error {
+							err := c.String(handlerResponseStatus, handlerResponseBody)
+							require.NoError(t, err)
+							return errors.New("handler abort")
+						},
+						test: func(t *testing.T, rec *httptest.ResponseRecorder, err error) {
+							require.Error(t, err)
+							require.Equal(t, "handler abort", err.Error())
+							require.Equal(t, handlerResponseStatus, rec.Code)
+							require.Equal(t, handlerResponseBody, rec.Body.String())
+						},
+					},
+
+					{
+						name: "sqreen after another middleware/no one aborts",
+						middlewares: []echo.MiddlewareFunc{
+							func(next echo.HandlerFunc) echo.HandlerFunc {
+								return func(c echo.Context) error {
+									err := c.String(middlewareResponseStatus, middlewareResponseBody)
+									require.NoError(t, err)
+									err = next(c)
+									require.NoError(t, err)
+									err = c.String(middlewareResponseStatus, middlewareResponseBody)
+									require.NoError(t, err)
+									return nil
+								}
+							},
+							middleware(tc.agent),
+						},
+						handler: func(c echo.Context) error {
+							err := c.String(handlerResponseStatus, handlerResponseBody)
+							require.NoError(t, err)
+							return nil
+						},
+						test: func(t *testing.T, rec *httptest.ResponseRecorder, err error) {
+							require.NoError(t, err)
+							require.Equal(t, middlewareResponseStatus, rec.Code)
+							require.Equal(t, middlewareResponseBody+handlerResponseBody+middlewareResponseBody, rec.Body.String())
+						},
+					},
+				} {
+					tc := tc
+					t.Run(tc.name, func(t *testing.T) {
+						// Create a echo router
+						router := echo.New()
+						// Setup the middleware
+						router.Use(tc.middlewares...)
+						// Add the endpoint
+						router.GET("/", tc.handler)
+
+						// Perform the request and record the output
+						rec := httptest.NewRecorder()
+						req, _ := http.NewRequest("GET", "/", nil)
+						var err error
+						router.HTTPErrorHandler = func(e error, _ echo.Context) {
+							err = e
+						}
+						router.ServeHTTP(rec, req)
+
+						// Check the request was performed as expected
+						tc.test(t, rec, err)
+					})
+				}
+			})
+		}
+	})
+
+	t.Run("response observation", func(t *testing.T) {
+		expectedStatusCode := 433
+
+		agent := &mockups.AgentMockup{}
+		agent.ExpectConfig().Return(&mockups.AgentConfigMockup{}).Once()
+		agent.ExpectIsIPAllowed(mock.Anything).Return(false).Once()
+		agent.ExpectIsPathAllowed(mock.Anything).Return(false).Once()
+		var responseStatusCode int
+		agent.ExpectSendClosedRequestContext(mock.MatchedBy(func(recorded types.ClosedRequestContextFace) bool {
+			resp := recorded.Response()
+			responseStatusCode = resp.Status()
+			return true
+		})).Return(nil)
+		defer agent.AssertExpectations(t)
+
+		// Create a route
+		router := echo.New()
+		router.Use(middleware(agent))
+		router.GET("/", func(c echo.Context) error {
+			return c.NoContent(expectedStatusCode)
+		})
+
+		// Perform the request and record the output
+		rec := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		var err error
+		router.HTTPErrorHandler = func(e error, _ echo.Context) {
+			err = e
+		}
+		router.ServeHTTP(rec, req)
+
+		// Check the result
+		require.NoError(t, err)
+		require.Equal(t, expectedStatusCode, responseStatusCode)
+		require.Equal(t, expectedStatusCode, rec.Code)
+	})
+
+}
+
+func middleware(agent protectioncontext.AgentFace) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			return middlewareHandler(agent, next, c)
+		}
+	}
+}
