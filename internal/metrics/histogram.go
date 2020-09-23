e74b9a036b253c1c4971226cc6a526169a6489e7
Julio Guerra <julio@sqreen.com>
2020-09-23 16:54:23 +0200

    agent/metrics: poc version integrating time and performance histograms

---
+++ internal/metrics/histogram.go
@@ -0,0 +1,367 @@
+// Copyright (c) 2016 - 2020 Sqreen. All Rights Reserved.
+// Please refer to our terms for more information:
+// https://www.sqreen.io/terms.html
+
+package metrics
+
+import (
+	"math"
+	"reflect"
+	"sort"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/sqreen/go-agent/internal/sqlib/sqassert"
+	"github.com/sqreen/go-agent/internal/sqlib/sqerrors"
+)
+
+// TimeHistogram is a metrics store optimized for write accesses to already existing
+// keys (cf. Add). It has a period of time after which the data is considered
+// ready to be retrieved. An empty store is never considered ready and the
+// deadline is computed when the first value is inserted.
+type TimeHistogram struct {
+	// Map of time buckets to a pointer to a sync.Map of comparable types to
+	// int64 pointers.
+	buckets sync.Map
+
+	// Minimum time duration the data should be kept.
+	period time.Duration
+
+	// Global RWMutex for exclusively locking the flush operation. This ensures
+	// the overall time coherence so that no writer from the past can happen after
+	// the flush has been done.
+	flushLock sync.RWMutex
+
+	// Once lock to update the start and deadline.
+	once sync.Once
+
+	// start time, aligned on the period duration, from which the first value was
+	// added.
+	start time.Time
+}
+
+func NewTimeHistogram(period time.Duration, maxLen uint) *TimeHistogram {
+	return &TimeHistogram{
+		period: period,
+	}
+}
+
+// Add delta to the given key, inserting it if it doesn't exist. This method
+// is thread-safe and optimized for updating existing key which is lock-free
+// when not concurrently retrieving (method `Flush()`) or inserting a new key.
+func (s *TimeHistogram) Add(key interface{}, delta int64) error {
+	// Avoid panic-ing by checking the key type is not nil and comparable.
+	if key == nil {
+		return sqerrors.New("unexpected key value `nil`")
+	}
+	if !reflect.TypeOf(key).Comparable() {
+		return sqerrors.Errorf("unexpected non-comparable type `%T`", key)
+	}
+
+	s.flushLock.RLock()
+	defer s.flushLock.RUnlock()
+
+	_, err := s.add(key, delta)
+
+	return err
+}
+
+func (s *TimeHistogram) add(key interface{}, delta int64) (uint64, error) {
+	s.once.Do(func() {
+		now := time.Now().Truncate(s.period)
+		s.start = now
+	})
+
+	bucket := s.bucket()
+
+	var store *sync.Map
+	if v, _ := s.buckets.Load(bucket); v != nil {
+		store = v.(*sync.Map)
+	} else {
+		store = &sync.Map{}
+		if actual, loaded := s.buckets.LoadOrStore(bucket, store); loaded {
+			store = actual.(*sync.Map)
+		}
+	}
+
+	// Fast hot path: concurrently updating the value of an existing key.
+	// Atomically update the value.
+	// This update operation can be therefore done concurrently.
+	actual, loaded := store.Load(key)
+	if !loaded {
+		actual, loaded = store.LoadOrStore(key, &delta)
+	}
+
+	if loaded {
+		// Atomically update the value.
+		sum := actual.(*int64)
+		atomic.AddInt64(sum, delta)
+	} // else this was the first value added
+
+	return bucket, nil
+}
+
+// Flush returns the stored data and the corresponding time window the data was
+// held. It should be used when the store is `Ready()`. This method is
+// thead-safe.
+func (s *TimeHistogram) Flush() (ready []ReadyStore) {
+	// Exclusively lock the store in order to get the values and replace it.
+	// No one else can be adding new data into the store
+	s.flushLock.Lock()
+	_, start, buckets := s.flushUnsafe()
+	// Unlock the store which is ready to be used again by concurrent goroutines.
+	s.flushLock.Unlock()
+
+	return makeReadyTimeHistogram(start, s.period, buckets)
+}
+
+func (s *TimeHistogram) flushUnsafe() (ongoing uint64, start time.Time, buckets sync.Map) {
+	// Save the ready histogram and its starting time
+	buckets = s.buckets
+	start = s.start
+
+	// Load the current time bucket values if any
+	var ongoingBucket *sync.Map
+	bucket := s.bucket()
+	if ongoing, loaded := buckets.Load(bucket); loaded {
+		// LoadAndDelete() is available from go1.15 - we can't use it for now
+		buckets.Delete(bucket)
+		ongoingBucket = ongoing.(*sync.Map)
+	}
+
+	// Reset the histogram
+	s.buckets = sync.Map{}
+	s.once = sync.Once{}
+	if ongoingBucket != nil {
+		// The current bucket is ongoing: set the start time and bucket list
+		// accordingly.
+		s.start = start.Add(time.Duration(bucket) * s.period)
+		s.buckets.Store(bucket, ongoingBucket)
+		ongoing = bucket
+	} else {
+		s.start = time.Time{}
+	}
+
+	return ongoing, start, buckets
+}
+
+func makeReadyTimeHistogram(start time.Time, period time.Duration, buckets sync.Map) (ready []ReadyStore) {
+	buckets.Range(func(k, v interface{}) bool {
+		bucket := k.(uint64)
+		st := start.Add(time.Duration(bucket) * period)
+
+		readyMap := make(ReadyStoreMap)
+		v.(*sync.Map).Range(func(k, v interface{}) bool {
+			readyMap[k] = *v.(*int64)
+			return true
+		})
+
+		ready = append(ready, &ReadyTimeHistogram{
+			timeBucket: bucket,
+			set:        readyMap,
+			start:      st,
+			finish:     st.Add(period),
+		})
+		return true
+	})
+
+	sort.Slice(ready, func(i, j int) bool {
+		return ready[i].Start().Before(ready[j].Start())
+	})
+
+	return ready
+}
+
+// Ready returns true when the store has values and the period passed.
+// This method is thread-safe. Note that the atomic operation
+// "Ready() + Flush()" doesn't exist, they should therefore be used by a single
+// "flusher" goroutine. The locking of `Ready()` is indeed weaker than `Flush()`
+// as it only lock the store for reading in order to avoid blocking other
+// concurrent updates.
+func (s *TimeHistogram) Ready() bool {
+	s.flushLock.RLock()
+	defer s.flushLock.RUnlock()
+	return !s.start.IsZero() && time.Since(s.start) >= s.period
+	//return !s.deadline.IsZero() && time.Now().After(s.deadline)
+}
+
+func (s *TimeHistogram) bucket() uint64 {
+	return uint64(time.Since(s.start) / s.period)
+}
+
+// ReadyTimeHistogram provides methods to get the values and the time window.
+type ReadyTimeHistogram struct {
+	set           ReadyStoreMap
+	start, finish time.Time
+	timeBucket    uint64
+}
+
+func (s *ReadyTimeHistogram) Start() time.Time {
+	return s.start
+}
+
+func (s *ReadyTimeHistogram) Finish() time.Time {
+	return s.finish
+}
+
+func (s *ReadyTimeHistogram) Metrics() ReadyStoreMap {
+	return s.set
+}
+
+type PerfHistogram struct {
+	unit, base float64
+	invLogBase float64
+	subParcel  float64
+
+	// The time histogram of performance buckets and their values.
+	timeHistogram *TimeHistogram
+
+	// Separate simplified time histogram of max values. It follows the same
+	// number of time buckets as the performance buckets'
+	maxValues sync.Map
+}
+
+func NewPerfHistogram(period time.Duration, unit, base float64, maxStoreLen uint) (*PerfHistogram, error) {
+	if unit <= 0.0 {
+		return nil, sqerrors.Errorf("unexpected binning unit value `%f`", unit)
+	}
+	if base <= 1.0 {
+		return nil, sqerrors.Errorf("unexpected binning base value `%f`", base)
+	}
+
+	sumStore := NewTimeHistogram(period, maxStoreLen)
+	logBase := math.Log(base)
+	logUnit := math.Log(unit)
+
+	return &PerfHistogram{
+		unit:          unit,
+		base:          base,
+		invLogBase:    1 / logBase,
+		subParcel:     logUnit / logBase,
+		timeHistogram: sumStore,
+	}, nil
+}
+
+func (s *PerfHistogram) Ready() bool {
+	return s.timeHistogram.Ready()
+}
+
+func (s *PerfHistogram) Add(v float64) error {
+	s.timeHistogram.flushLock.RLock()
+	defer s.timeHistogram.flushLock.RUnlock()
+
+	perfBucket := s.bucket(v)
+	// TODO: pass the current time now to use
+	timeBucket, err := s.timeHistogram.add(perfBucket, 1)
+	if err != nil {
+		return err
+	}
+
+	s.updateMax(timeBucket, v)
+	return nil
+}
+
+func (s *PerfHistogram) bucket(v float64) (bin uint64) {
+	if v < s.unit {
+		return 1
+	}
+	r := math.Floor(math.Log(v)*s.invLogBase - s.subParcel)
+	sqassert.True(r > 0 && !math.IsNaN(r) && math.IsInf(r, 0))
+	return 2 + uint64(r)
+}
+
+// Lock-less update of the max value using a compare-and-swap loop.
+func (s *PerfHistogram) updateMax(timeBucket uint64, v float64) {
+	// Fast path: try to load the max pointer first. This allows to only use
+	// LoadOrStore and its pointer allocation for every call to updateMax.
+	maxBitsPtrFace, loaded := s.maxValues.Load(timeBucket)
+	if !loaded {
+		// Slow path
+		maxBits := math.Float64bits(v)
+		maxBitsPtr := &maxBits
+		// Note this involves an allocation we want to avoid when not necessary,
+		// hence the first use of Load() alone to.
+		maxBitsPtrFace, loaded = s.maxValues.LoadOrStore(timeBucket, maxBitsPtr)
+		if !loaded {
+			// First value
+			return
+		}
+	}
+	maxBitsPtr := maxBitsPtrFace.(*uint64)
+
+	// Load the current max value and try to update it when the new value is
+	// bigger by using the CAS operation. When successfully swapped, we can return
+	// from the function. But if not, we need to retry by reloading the current
+	// value and checking again if the new value is the new max, and retry again
+	// if necessary.
+	vBits := math.Float64bits(v)
+	for {
+		maxBits := atomic.LoadUint64(maxBitsPtr)
+		max := math.Float64frombits(maxBits)
+		if v <= max {
+			// This value `v` is smaller than the current max value.
+			return
+		}
+
+		// This value `v` is greater than the current max value. Therefore, we try
+		// to CAS it.
+		if swapped := atomic.CompareAndSwapUint64(maxBitsPtr, maxBits, vBits); swapped {
+			// Successfully swapped
+			break
+		}
+
+		// Not swapped - retry everything
+	}
+}
+
+func (s *PerfHistogram) Flush() (ready []ReadyStore) {
+	start, timeBuckets, maxValues := s.flush()
+
+	timeHist := makeReadyTimeHistogram(start, s.timeHistogram.period, timeBuckets)
+
+	for _, timeHist := range timeHist {
+		timeHist := timeHist.(*ReadyTimeHistogram)
+		v, ok := maxValues.Load(timeHist.timeBucket)
+		sqassert.True(ok)
+		// TODO: write a helper func to access max values
+		max := math.Float64frombits(*v.(*uint64))
+
+		ready = append(ready, &ReadyPerfHistogram{
+			ReadyTimeHistogram: timeHist,
+			max:                max,
+			base:               s.base,
+			unit:               s.unit,
+		})
+
+	}
+	return
+}
+
+func (s *PerfHistogram) flush() (start time.Time, timeBuckets, maxValuesTimeBucket sync.Map) {
+	s.timeHistogram.flushLock.Lock()
+	defer s.timeHistogram.flushLock.Unlock()
+
+	ongoing, start, timeBuckets := s.timeHistogram.flushUnsafe()
+	maxValuesTimeBucket = s.maxValues
+
+	s.maxValues = sync.Map{}
+	if ongoing > 0 {
+		// TODO: period 1' - add 10000 + flush + add 100 -> test prochain flush doit donner 10000
+		v, ok := maxValuesTimeBucket.Load(ongoing)
+		sqassert.True(ok)
+		maxValuesTimeBucket.Delete(ongoing)
+		s.maxValues.Store(ongoing, v)
+	}
+
+	//maxValues = map[uint64]float64{}
+	//timeBuckets.Range(func(k, _ interface{}) bool {
+	//	v, ok := s.maxValues.Load(k)
+	//	sqassert.True(ok)
+	//	// No need for atomic load of v as we are in the exclusive lock section
+	//	maxValues[k.(uint64)] = math.Float64frombits(*v.(*uint64))
+	//	return true
+	//})
+
+	return start, timeBuckets, maxValuesTimeBucket
+}
--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -51,29 +51,21 @@ package metrics
 
 import (
 	"fmt"
-	"math"
-	"reflect"
 	"sync"
-	"sync/atomic"
 	"time"
-
-	"github.com/sqreen/go-agent/internal/plog"
-	"github.com/sqreen/go-agent/internal/sqlib/sqassert"
-	"github.com/sqreen/go-agent/internal/sqlib/sqerrors"
 )
 
 // Engine manages the metrics stores in oder to create new one, and to poll
 // the existing ones. Engine's methods are not thread-safe and designed to be
 // used by a single goroutine.
 type Engine struct {
-	logger             plog.DebugLogger
-	stores             map[string]Store
-	maxMetricsStoreLen uint
+	stores map[string]Store
+	lock   sync.RWMutex
 }
 
 type Store interface {
 	Ready() bool
-	Flush() ReadyStore
+	Flush() []ReadyStore
 }
 
 type ReadyStore interface {
@@ -82,41 +74,75 @@ type ReadyStore interface {
 	Metrics() ReadyStoreMap
 }
 
-func NewEngine(logger plog.DebugLogger, maxMetricsStoreLen uint) *Engine {
+func NewEngine() *Engine {
 	return &Engine{
-		logger:             logger,
-		stores:             make(map[string]Store),
-		maxMetricsStoreLen: maxMetricsStoreLen,
+		stores: make(map[string]Store),
 	}
 }
 
-// GetSumStore creates and registers a new metrics store when it does not exist. It
+// TimeHistogram creates and registers a new metrics store when it does not exist. It
 // returns the existing one otherwise.
-func (e *Engine) GetSumStore(id string, period time.Duration) *SumStore {
-	// TODO: rwlock e.stores
-	if store, exists := e.stores[id]; exists {
-		return store.(*SumStore)
+func (e *Engine) TimeHistogram(id string, period time.Duration) *TimeHistogram {
+	if store := e.getTimeHistogram(id); store != nil {
+		return store
 	}
-	store := newStore(period, e.maxMetricsStoreLen)
+
+	e.lock.Lock()
+	defer e.lock.Unlock()
+
+	if store := e.getTimeHistogramUnsafe(id); store != nil {
+		return store
+	}
+
+	store := NewTimeHistogram(period, 0)
 	e.stores[id] = store
 	return store
 }
 
-func (e *Engine) GetBinningStore(id string, unit, base float64, period time.Duration) (*BinningStore, error) {
+func (e *Engine) getTimeHistogram(id string) *TimeHistogram {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	return e.getTimeHistogramUnsafe(id)
+}
+
+func (e *Engine) getTimeHistogramUnsafe(id string) *TimeHistogram {
 	if store, exists := e.stores[id]; exists {
-		return store.(*BinningStore), nil
+		return store.(*TimeHistogram)
+	}
+	return nil
+}
+
+func (e *Engine) PerfHistogram(id string, unit, base float64, period time.Duration) (*PerfHistogram, error) {
+	if store := e.getPerfHistogram(id); store != nil {
+		return store, nil
 	}
 
-	if unit <= 0.0 {
-		return nil, sqerrors.Errorf("unexpected binning unit value `%f`", unit)
-	}
-	if base <= 1.0 {
-		return nil, sqerrors.Errorf("unexpected binning base value `%f`", base)
+	e.lock.Lock()
+	defer e.lock.Unlock()
+
+	if store := e.getPerfHistogramUnsafe(id); store != nil {
+		return store, nil
 	}
 
-	store := newBinningStore(period, unit, base, e.maxMetricsStoreLen)
+	store, err := NewPerfHistogram(period, unit, base, 0)
+	if err != nil {
+		return nil, err
+	}
 	e.stores[id] = store
 	return store, nil
+}
+
+func (e *Engine) getPerfHistogram(id string) *PerfHistogram {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	return e.getPerfHistogramUnsafe(id)
+}
+
+func (e *Engine) getPerfHistogramUnsafe(id string) *PerfHistogram {
+	if store, exists := e.stores[id]; exists {
+		return store.(*PerfHistogram)
+	}
+	return nil
 }
 
 // ReadyMetrics returns the set of ready stores (ie. having data and a passed
@@ -126,9 +152,9 @@ func (e *Engine) ReadyMetrics() (expiredMetrics map[string]ReadyStore) {
 	expiredMetrics = make(map[string]ReadyStore)
 	for id, s := range e.stores {
 		if s.Ready() {
-			ready := s.Flush()
-			expiredMetrics[id] = ready
-			e.logger.Debugf("metrics: store `%s` ready with `%d` entries", id, len(ready.Metrics()))
+			for _, ready := range s.Flush() {
+				expiredMetrics[id] = ready
+			}
 		}
 	}
 	if len(expiredMetrics) == 0 {
@@ -137,126 +163,18 @@ func (e *Engine) ReadyMetrics() (expiredMetrics map[string]ReadyStore) {
 	return expiredMetrics
 }
 
-// SumStore is a metrics store optimized for write accesses to already existing
-// keys (cf. Add). It has a period of time after which the data is considered
-// ready to be retrieved. An empty store is never considered ready and the
-// deadline is computed when the first value is inserted.
-type SumStore struct {
-	// Map of comparable types to uint64 pointers.
-	set  StoreMap
-	lock sync.RWMutex
-	// Next deadline, computed when the first value is inserted.
-	deadline time.Time
-	// Minimum time duration the data should be kept.
-	period time.Duration
-	// Maximum map length. New keys are dropped when reached.
-	// The length is unlimited when 0.
-	maxLen uint
-}
-
-type BinningStore struct {
-	unit, base float64
-	invLogBase float64
-	subParcel  float64
-	// The max value is a uint64 value so that we can assign it with a lock-less
-	// operation using atomic integer operations.
-	maxValue int64
-	// The store of bins and their values. The existing sum store is suitable.
-	sumStore *SumStore
-	// Lock to atomically flush the store
-	rwlock sync.RWMutex
-}
-
-func newBinningStore(period time.Duration, unit, base float64, maxStoreLen uint) *BinningStore {
-	sumStore := newStore(period, maxStoreLen)
-	logBase := math.Log(base)
-	logUnit := math.Log(unit)
-
-	return &BinningStore{
-		unit:       unit,
-		base:       base,
-		invLogBase: 1 / logBase,
-		subParcel:  logUnit / logBase,
-		sumStore:   sumStore,
-	}
-}
-
-func (s *BinningStore) Ready() bool {
-	return s.sumStore.Ready()
-}
-
-type ReadyBinningStore struct {
-	*ReadySumStore
-	max        int64
+type ReadyPerfHistogram struct {
+	*ReadyTimeHistogram
+	max        float64
 	base, unit float64
 }
 
-func (s *ReadyBinningStore) Unit() float64 { return s.unit }
-func (s *ReadyBinningStore) Base() float64 { return s.base }
-func (s *ReadyBinningStore) Max() int64    { return s.max }
-
-func (s *BinningStore) Flush() ReadyStore {
-	s.rwlock.Lock()
-	defer s.rwlock.Unlock()
-
-	max := atomic.LoadInt64(&s.maxValue)
-	atomic.StoreInt64(&s.maxValue, 0)
-	sumStore := s.sumStore.Flush().(*ReadySumStore)
-	return &ReadyBinningStore{
-		ReadySumStore: sumStore,
-		max:           max,
-		base:          s.base,
-		unit:          s.unit,
-	}
-}
-
-func (s *BinningStore) Add(v float64) error {
-	s.rwlock.RLock()
-	defer s.rwlock.RUnlock()
-
-	// Updating the max value only with the integer part, assuming its unit is
-	// enough to be interesting (eg. milliseconds vs microseconds).
-	s.updateMax(int64(math.Floor(v)))
-	return s.sumStore.Add(s.bin(v), 1)
-}
-
-func (s *BinningStore) bin(v float64) (bin uint64) {
-	if v < s.unit {
-		return 1
-	}
-	r := math.Floor(math.Log(v)*s.invLogBase - s.subParcel)
-	sqassert.True(r > 0 && !math.IsNaN(r) && math.IsInf(r, 0))
-	return 2 + uint64(r)
-}
-
-// Lock-less update of the max value using a compare-and-swap loop.
-func (s *BinningStore) updateMax(v int64) {
-	// Load the current max value and try to update it when the new value is
-	// bigger by using the CAS operation. When successfully swapped, we can return
-	// from the function. But if not, we need to retry by reloading the current
-	// value and checking again if the new value is the new max, and retry again
-	// if necessary.
-	for {
-		current := atomic.LoadInt64(&s.maxValue)
-		if v <= current {
-			break
-		}
-		if swapped := atomic.CompareAndSwapInt64(&s.maxValue, current, v); swapped {
-			break
-		}
-	}
-}
+func (s *ReadyPerfHistogram) Unit() float64 { return s.unit }
+func (s *ReadyPerfHistogram) Base() float64 { return s.base }
+func (s *ReadyPerfHistogram) Max() float64  { return s.max }
 
 type StoreMap map[interface{}]*int64
 type ReadyStoreMap map[interface{}]int64
-
-func newStore(period time.Duration, maxLen uint) *SumStore {
-	return &SumStore{
-		set:    make(StoreMap),
-		period: period,
-		maxLen: maxLen,
-	}
-}
 
 type MaxMetricsStoreLengthError struct {
 	MaxLen uint
@@ -264,126 +182,4 @@ type MaxMetricsStoreLengthError struct {
 
 func (e MaxMetricsStoreLengthError) Error() string {
 	return fmt.Sprintf("new metrics store key dropped as the metrics store has reached its maximum length `%d`", e.MaxLen)
-}
-
-// Add delta to the given key, inserting it if it doesn't exist. This method
-// is thread-safe and optimized for updating existing key which is lock-free
-// when not concurrently retrieving (method `Flush()`) or inserting a new key.
-func (s *SumStore) Add(key interface{}, delta int64) error {
-	// Avoid panic-ing by checking the key type is not nil and comparable.
-	if key == nil {
-		return sqerrors.New("unexpected key value `nil`")
-	}
-	if !reflect.TypeOf(key).Comparable() {
-		return sqerrors.Errorf("unexpected non-comparable type `%T`", key)
-	}
-
-	// Fast hot path: concurrently updating the value of an existing key.
-	// Lock the store for reading only.
-	s.lock.RLock()
-	// Lookup the value
-	value, exists := s.set[key]
-	if exists {
-		// The key already exists.
-		// Atomically update the value.
-		// This update operation can be therefore done concurrently.
-		atomic.AddInt64(value, delta)
-		// It is important to do it in this write-safe section that is mutually
-		// exclusive with Flush() which replaces the store's map using Lock().
-	}
-	// Unlock the store
-	s.lock.RUnlock()
-
-	// Slow path: the key does not exist
-	if !exists {
-		// Exclusively lock the store
-		s.lock.Lock()
-		defer s.lock.Unlock()
-		// Check again in case the value has been inserted while getting here.
-		value, exists = s.set[key]
-		if exists {
-			// The value was inserted by another concurrent goroutine.
-			// We can update the value without atomic operation as we exclusively
-			// have the lock.
-			*value += delta
-			// Note that this is not possible to unlock and perform the atomic
-			// operation because of possible concurrent `Flush()`.
-		} else {
-			if l := len(s.set); l == 0 {
-				// Set the deadline when this is the first value inserted into the store
-				s.deadline = time.Now().Add(s.period)
-			} else if s.maxLen > 0 && uint(l) >= s.maxLen {
-				// The maximum length is reached - no more new insertions are allowed
-				return MaxMetricsStoreLengthError{MaxLen: s.maxLen}
-			}
-			// The value still doesn't exist and we need to insert it into the store's
-			// map.
-			value := delta
-			s.set[key] = &value
-		}
-	}
-
-	return nil
-}
-
-// Flush returns the stored data and the corresponding time window the data was
-// held. It should be used when the store is `Ready()`. This method is
-// thead-safe.
-func (s *SumStore) Flush() (flushed ReadyStore) {
-	// Read current time before swapping the stores in order to avoid making it in
-	// the critical-section. Reading it before is important in order to get
-	// old.finish <= new.start.
-	now := time.Now()
-
-	// Exclusively lock the store in order to get the values and replace it.
-	s.lock.Lock()
-	oldMap := s.set
-	startedAt := s.deadline.Add(-s.period)
-	// Create a new map with the same capacity as the old one to avoid allocation
-	// time when still used the same way after the flush.
-	s.set = make(StoreMap, len(oldMap))
-	s.deadline = time.Time{} // time.Time zero value
-	// Unlock the store which is ready to be used again by concurrent goroutines.
-	s.lock.Unlock()
-
-	// Compute the map of values getting rid of the pointers (less GC-pressure).
-	readyMap := make(ReadyStoreMap, len(oldMap))
-	for k, v := range oldMap {
-		readyMap[k] = *v
-	}
-	return &ReadySumStore{
-		set:    readyMap,
-		start:  startedAt,
-		finish: now,
-	}
-}
-
-// Ready returns true when the store has values and the period passed.
-// This method is thread-safe. Note that the atomic operation
-// "Ready() + Flush()" doesn't exist, they should therefore be used by a single
-// "flusher" goroutine. The locking of `Ready()` is indeed weaker than `Flush()`
-// as it only lock the store for reading in order to avoid blocking other
-// concurrent updates.
-func (s *SumStore) Ready() bool {
-	s.lock.RLock()
-	defer s.lock.RUnlock()
-	return !s.deadline.IsZero() && time.Now().After(s.deadline)
-}
-
-// ReadySumStore provides methods to get the values and the time window.
-type ReadySumStore struct {
-	set           ReadyStoreMap
-	start, finish time.Time
-}
-
-func (s *ReadySumStore) Start() time.Time {
-	return s.start
-}
-
-func (s *ReadySumStore) Finish() time.Time {
-	return s.finish
-}
-
-func (s *ReadySumStore) Metrics() ReadyStoreMap {
-	return s.set
 }
